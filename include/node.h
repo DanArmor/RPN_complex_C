9030b1ececd9f85fa880894efddaa7468ebb7fc5
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-06-20 02:49:03 +0300

    feat: ref, copy and own nodes

--- include/node.h
+++ include/node.h
@@ -1,13 +1,23 @@
 #ifndef NODE_H
 #define NODE_H
+
+#define NODE_REF 1
+#define NODE_OWN 2
+
 typedef struct Node{
+    int type;
     void *data;
     struct Node *link;
 } Node;
 
 Node *new_node();
-Node *pack_data(void *p_data);
+// Создание узла, который контролирует ресурс
+Node *own_data(void *p_data);
+// Создание узла, который ссылается на ресурс, время жизни которого он не контролирует
+Node *ref_data(void *p_data);
+
 void *unpack_data(Node *p_node);
+// Нельзя применять на узлах, которые не владеют значением внутри себя
 void destroy_node(Node *p_node);
 
 #endif
--- src/node.c
+++ src/node.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "node.h"
 
 Node *new_node(){
@@ -8,9 +9,18 @@ Node *new_node(){
     return p_node;
 }
 
-Node *pack_data(void *p_data){
+Node *own_data(void *p_data){
     Node *p_node = new_node();
+    p_node->type = NODE_OWN;
     p_node->data = p_data;
+    return p_node;
+}
+
+Node *copy_data(void *p_data, int size){
+    Node *p_node = new_node();
+    p_node->type = NODE_OWN;
+    p_node->data = (void*)calloc(1, size);
+    memcpy(p_node->data, p_data, size);
     return p_node;
 }
 
@@ -20,8 +30,15 @@ void *unpack_data(Node *p_node){
     return p_data;
 }
 
+Node *ref_data(void *p_data){
+    Node *p_node = new_node();
+    p_node->type = NODE_REF;
+    p_node->data = p_data;
+    return p_node;
+}
+
 void destroy_node(Node *p_node) {
-    if(p_node->data != NULL) {
+    if(p_node->type == NODE_OWN && p_node->data != NULL) {
         free(p_node->data);
     }
     free(p_node);
