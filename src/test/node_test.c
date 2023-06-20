ad284d04b1978e77c54b6479dc6f9a6bf96a9b94
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-06-20 03:04:15 +0300

    feat: node tests

--- src/test/node_test.c
+++ src/test/node_test.c
@@ -1,4 +1,7 @@
+#include <signal.h>
+
 #include "node.h"
+
 
 #include "criterion/criterion.h"
 
@@ -16,4 +19,19 @@ Test(node, ref_data) {
     destroy_node(p_node);
 }
 
-Test(node, copy_data) {}
+Test(node, copy_data) {
+    int a = 3;
+    Node *p_node = copy_data(&a, sizeof(int));
+    cr_assert(p_node->type == NODE_OWN);
+    cr_assert(*(int *)(p_node->data) == a);
+    destroy_node(p_node);
+}
+
+Test(node, own_data) {
+    int *a = calloc(1, sizeof(int));
+    *a = 3;
+    Node *p_node = own_data(a);
+    cr_assert(p_node->type == NODE_OWN);
+    cr_assert(*(int *)(p_node->data) == *a);
+    destroy_node(p_node);
+}
